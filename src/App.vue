059cacde2e161183e6ce1f6db7029506e65d2bf5
zulfadhli <zulfadhli4141@gmail.com>
2018-02-24 15:29:38 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home'
+
+export default {
+  name: 'app',
+  components: {
+    Home
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App!'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  #app {
+    color: #56b983;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <button class="px-4 py-6 text-white bg-green hover:bg-green-dark rounded mr-4">Vue</button>
+    <button class="px-4 py-6 text-white bg-teal hover:bg-teal-dark rounded mr-4">Tailwind</button>
+    <button class="px-4 py-6 text-white bg-orange hover:bg-orange-dark rounded">Parcel</button>
+  </div>
+</template>
